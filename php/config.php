e177df237dbbcb4d9b8b94efa3df0a44babff7a9
GERARD XAVIER FAGUNDEZ BERON <55315361@ceibal.edu.uy>
2026-08-17 17:02:47 -0300

    save21

---
+++ php/config.php
@@ -0,0 +1,18 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_PORT', 3306);
+define('DB_NAME', 'hospital de clinicas');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('BASE_URL', 'http://localhost/hospital');
+
+$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
+if ($con->connect_error) {
+    die('ERROR al conectar: ' . $con->connect_error);
+}
+
+$con->set_charset('utf8');
+
+?>

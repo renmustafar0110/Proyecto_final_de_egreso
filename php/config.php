1b06dc07b47a0f392f7a2ccac400178d397582e4
renmustafar0110 <renskywalker456@gmail.com>
2026-08-31 20:09:32 -0300

    save32

--- php/config.php
+++ php/config.php
@@ -1,11 +1,9 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
 define('DB_NAME', 'hospital_clinicas');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('BASE_URL', 'http://localhost/hospital');
 
 $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 

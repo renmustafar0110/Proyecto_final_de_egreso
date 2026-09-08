c766fc84b9d8097c73fd16baaeb3d530e23c773c
renmustafar0110 <renskywalker456@gmail.com>
2026-09-08 13:59:44 -0300

    save39

--- php/cargaDocumentos.php
+++ php/cargaDocumentos.php
@@ -6,19 +6,17 @@ $archivo = $_FILES['archivo'];
 $nombre = $_POST['nombre'];
 
 if ($archivo['error'] === 0) {
-    $ruta = "documento/" . $archivo['name'];
+    $ruta = "Documento/" . $archivo['name'];
     move_uploaded_file($archivo['tmp_name'], $ruta);
 
-    $sql = "INSERT INTO documento (nombre, archivo)
+    $sql = "INSERT INTO Documento (nombre, archivo)
             VALUES ('$nombre', '$ruta')";
 
-    if ($con->query($sql)) {
+    if ($conexion->query($sql)) {
         echo "Registro exitoso";
     } else {
         echo "Falló el registro";
     }
 }
 
-$con->close();
-
-?>
+$conexion->close();
--- php/conexion.php
+++ php/conexion.php
@@ -5,12 +5,12 @@ define('DB_NAME', 'hospital_clinicas');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-$con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-if ($con->connect_error) {
-    die('ERROR al conectar: ' . $con->connect_error);
+if ($conexion->connect_error) {
+    die('ERROR al conectar: ' . $conexion->connect_error);
 }
 
-$con->set_charset('utf8');
+$conexion->set_charset('utf8');
 
 ?>
